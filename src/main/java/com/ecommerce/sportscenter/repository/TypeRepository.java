2435aaf51e1c4a663bcb6fd9d5ce4bf50cfa2339
Prakhar Kumar <kumarprakhar1201@gmail.com>
2024-08-06 22:02:12 +0530

    First Commit

---
+++ src/main/java/com/ecommerce/sportscenter/repository/TypeRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.sportscenter.repository;
+
+import com.ecommerce.sportscenter.entity.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRepository extends JpaRepository<Type, Integer> {
+}
