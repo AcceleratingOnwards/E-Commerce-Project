a981fc49156e7f82dffcc8015df820be23580322
Prakhar Kumar <kumarprakhar1201@gmail.com>
2024-08-11 19:27:19 +0530

    First Commit

--- src/main/java/com/ecommerce/sportscenter/controller/ProductController.java
+++ src/main/java/com/ecommerce/sportscenter/controller/ProductController.java
@@ -6,12 +6,14 @@ import com.ecommerce.sportscenter.model.TypeResponse;
 import com.ecommerce.sportscenter.service.BrandService;
 import com.ecommerce.sportscenter.service.ProductService;
 import com.ecommerce.sportscenter.service.TypeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,8 +36,21 @@ public class ProductController {
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
     @GetMapping()
-    public ResponseEntity<List<ProductResponse>> getProducts(){
-        List<ProductResponse> productResponses = productService.getProducts();
+    public ResponseEntity<Page<ProductResponse>> getProducts(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size,
+            @RequestParam(name = "keyword", required = false) String keyword,
+            @RequestParam(name = "brandId", required = false) Integer brandId,
+            @RequestParam(name = "typeId", required = false) Integer typeId,
+            @RequestParam(name = "sort", defaultValue = "name") String sort,
+            @RequestParam(name = "order", defaultValue = "asc") String order
+    ){
+        //Convert order to Sort direction
+        Sort.Direction direction = order.equalsIgnoreCase("desc") ? Sort.Direction.DESC: Sort.Direction.ASC;
+        Sort sorting = Sort.by(direction, sort);
+        Pageable pageable = PageRequest.of(page, size, sorting);
+
+        Page<ProductResponse> productResponses = productService.getProducts(pageable, brandId, typeId, keyword);
         return new ResponseEntity<>(productResponses, HttpStatus.OK);
     }
     @GetMapping("/brands")
--- src/main/java/com/ecommerce/sportscenter/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/sportscenter/service/ProductServiceImpl.java
@@ -4,6 +4,9 @@ import com.ecommerce.sportscenter.entity.Product;
 import com.ecommerce.sportscenter.model.ProductResponse;
 import com.ecommerce.sportscenter.repository.ProductRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,18 +32,23 @@ public class ProductServiceImpl implements ProductService{
         return productResponse;
     }
 
-
     @Override
-    public List<ProductResponse> getProducts() {
-        log.info("Fetching Products!!!");
-        //Fetching from DB
-        List<Product> productList = productRepository.findAll();
-        //Map
-        List<ProductResponse> productResponses = productList.stream()
-                .map(this::convertToProductResponse)
-                .collect(Collectors.toList());
-        log.info("Fetched All Products!!!");
-        return productResponses;
+    public Page<ProductResponse> getProducts(Pageable pageable, Integer brandId, Integer typeId, String keyword) {
+        Specification<Product> spec = Specification.where(null);
+
+        if (brandId != null) {
+            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("brand").get("id"), brandId));
+        }
+
+        if (typeId != null) {
+            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("type").get("id"), typeId));
+        }
+
+        if (keyword != null && !keyword.isEmpty()) {
+            spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("name"), "%" + keyword + "%"));
+        }
+
+        return productRepository.findAll(spec, pageable).map(this::convertToProductResponse);
     }
     private ProductResponse convertToProductResponse(Product product) {
         return ProductResponse.builder()
