98aeed4c664974e4e07fb045e709d18b14ed28ce
Prakhar Kumar <kumarprakhar1201@gmail.com>
2024-08-13 13:18:38 +0530

    React application

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+import { Product } from "../models/product";
+
+function App() {
+  //Define a state variable products, using useState
+  const [products, setProducts] = useState<Product []>([]);
+  // useEffect(()=>{
+  //   //Function to fetch the data
+  //   const fetchData = async () =>{
+  //     try{
+  //       const response = await fetch('http://localhost:8081/api/products');
+  //       if(!response.ok){
+  //         throw new Error('Failed to fetch the data');
+  //       }
+  //       const data = await response.json();
+  //       setProducts(data.content);
+  //     }catch(error){
+  //       console.error('Error Fetching Data:', error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+  useEffect(()=>{
+    fetch('http://localhost:8081/api/products')
+    .then(response => response.json())
+    .then(data=> setProducts(data.content));
+  },[]);
+
+  return (
+    <div>
+      <h1>Sports Center</h1>
+      {products.map(product=>(
+        <div key={product.id}>
+          <p>Name: {product.name}</p>
+          <p>Description: {product.description}</p>
+          <p>Price: ${product.price}</p>
+          <p>Brand: {product.productBrand}</p>
+          <p>Type: {product.productType}</p>
+        </div>
+      ))}
+      
+    </div>
+  )
+}
+
+export default App
