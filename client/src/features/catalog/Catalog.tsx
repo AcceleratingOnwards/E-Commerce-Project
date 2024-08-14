305a683d0d27750d2a030de59d1ad70cc2af634d
Prakhar Kumar <kumarprakhar1201@gmail.com>
2024-08-14 15:37:26 +0530

    Adding React Componenets

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
+
+export default function Catalog(){
+    const [products, setProducts] = useState<Product []>([]);
+    useEffect(()=>{
+        fetch('http://localhost:8081/api/products')
+        .then(response => response.json())
+        .then(data=> setProducts(data.content));
+      },[]);
+    
+      return (
+        <>
+          <ProductList products={products}/>
+        </>
+      )
+
+}
+
