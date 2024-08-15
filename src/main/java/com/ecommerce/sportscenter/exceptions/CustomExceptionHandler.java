3ca781e9a1478a1938b11f8a393281e2c9842ccf
Prakhar Kumar <kumarprakhar1201@gmail.com>
2024-08-15 21:52:18 +0530

    Implementaing Best Practices: centralizing API , implementing error interceptors, toast notification, custom-error, spinner

---
+++ src/main/java/com/ecommerce/sportscenter/exceptions/CustomExceptionHandler.java
@@ -0,0 +1,2 @@
+package com.ecommerce.sportscenter.exceptions;public class CustomExceptionHandler {
+}
---
+++ src/main/java/com/ecommerce/sportscenter/exceptions/ProductNotFoundException.java
@@ -0,0 +1,4 @@
+package com.ecommerce.sportscenter.exception;
+
+public class ProductNotFoundException {
+}
