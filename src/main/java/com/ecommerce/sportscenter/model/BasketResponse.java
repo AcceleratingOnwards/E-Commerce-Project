1a57587d04e9aff4950568a553c26f015c1e9973
Prakhar Kumar <kumarprakhar1201@gmail.com>
2024-08-16 12:52:06 +0530

    adding basket Functionality-server side

--- src/main/java/com/ecommerce/sportscenter/model/BasketResponse.java
+++ src/main/java/com/ecommerce/sportscenter/model/BasketResponse.java
@@ -1,2 +1,17 @@
-package com.ecommerce.sportscenter.model;public class BasketResponse {
+package com.ecommerce.sportscenter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BasketResponse {
+    private String id;
+    private List<BasketItemResponse> items;
 }
--- src/main/java/com/ecommerce/sportscenter/repository/BasketRepository.java
+++ src/main/java/com/ecommerce/sportscenter/repository/BasketRepository.java
@@ -1,2 +1,9 @@
-package com.ecommerce.sportscenter.repository;public class BasketRepository {
+package com.ecommerce.sportscenter.repository;
+
+import com.ecommerce.sportscenter.entity.Basket;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BasketRepository extends CrudRepository<Basket, String> {
 }
